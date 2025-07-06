798a745442bff4a6be86844ce5d30e950e7312d7
iamgiauy <giauylethe@gmail.com>
2025-07-06 23:31:10 +0700

    vnpay api

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const fetch = require('node-fetch');    
 const crypto = require('crypto');
 const FormData = require('form-data');  
+const dateFormat = require('dateformat');
+const config = require('config');
 const verifyToken = require('./firebaseAuthencation');
 admin.initializeApp({
   credential: admin.credential.cert({
@@ -39,6 +41,61 @@ function randomString(length = 10) {
   }
   return result;
 }
+// vnpay api
+app.post('/create-payment-url', async (req,res) =>
+  {
+    const ipAddr = req.headers['x-forwarded-for'] ||
+            req.connection.remoteAddress ||
+            req.socket.remoteAddress ||
+            req.connection.socket.remoteAddress;
+       
+        var tmnCode = config.get('vnp_TmnCode');
+        var secretKey = config.get('vnp_HashSecret');
+        var vnpUrl = config.get('vnp_Url');
+        var returnUrl = config.get('vnp_ReturnUrl');
+    
+        var date = new Date();
+    
+        var createDate = dateFormat(date, 'yyyymmddHHmmss');
+        var orderId = dateFormat(date, 'HHmmss');
+      var amount = req.body.amount;
+        var bankCode = req.body.bankCode;
+        
+        var orderInfo = req.body.orderDescription;
+        var orderType = req.body.orderType;
+        var locale = req.body.language;
+        if(locale === null || locale === ''){
+            locale = 'vn';
+        }
+        var currCode = 'VND';
+        var vnp_Params = {};
+        vnp_Params['vnp_Version'] = '2.1.0';
+        vnp_Params['vnp_Command'] = 'pay';
+        vnp_Params['vnp_TmnCode'] = tmnCode;
+        vnp_Params['vnp_Locale'] = locale;
+        vnp_Params['vnp_CurrCode'] = currCode;
+        vnp_Params['vnp_TxnRef'] = orderId;
+        vnp_Params['vnp_OrderInfo'] = orderInfo;
+        vnp_Params['vnp_OrderType'] = orderType;
+        vnp_Params['vnp_Amount'] = amount * 100;
+        vnp_Params['vnp_ReturnUrl'] = returnUrl;
+        vnp_Params['vnp_IpAddr'] = ipAddr;
+        vnp_Params['vnp_CreateDate'] = createDate;
+        if(bankCode !== null && bankCode !== ''){
+            vnp_Params['vnp_BankCode'] = bankCode;
+        }
+        vnp_Params = sortObject(vnp_Params);
+        var querystring = require('qs');
+        var signData = querystring.stringify(vnp_Params, { encode: false });
+        var crypto = require("crypto");     
+        var hmac = crypto.createHmac("sha512", secretKey);
+        var signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex"); 
+        vnp_Params['vnp_SecureHash'] = signed;
+        vnpUrl += '?' + querystring.stringify(vnp_Params, { encode: false });
+        res.redirect(vnpUrl)
+    });
+    
+  });
 app.get('/charge/callback',async (req,res) => {
   try
   {
