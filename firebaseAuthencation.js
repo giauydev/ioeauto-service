49f7fe40fc1ff0914596bbf13307e9707d70285b
iamgiauy <giauylethe@gmail.com>
2025-07-06 07:32:30 +0700

    Create firebaseAuthencation.js

---
+++ firebaseAuthencation.js
@@ -0,0 +1,17 @@
+async function verifyToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).send('Thiếu token');
+  }
+
+  const idToken = authHeader.split(' ')[1];
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    req.uid = decodedToken.uid;
+    next();
+  } catch (error) {
+    return res.status(401).send('Token không hợp lệ');
+  }
+}
+
+module.exports = verifyToken;
