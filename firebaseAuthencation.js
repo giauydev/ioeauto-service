d1f2bf650042143aa9af2b4a8338b4120e9207ed
iamgiauy <giauylethe@gmail.com>
2025-07-09 20:51:54 +0700

    Update firebaseAuthencation.js

--- firebaseAuthencation.js
+++ firebaseAuthencation.js
@@ -1,12 +1,3 @@
-const admin = require('firebase-admin');
-
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
-  })
-});
 const db = admin.firestore(); 
 async function verifyToken(req, res, next) {
   const authHeader = req.headers.authorization;
@@ -25,7 +16,7 @@ async function verifyToken(req, res, next) {
       .limit(1)
       .get();
     const userData = userSnap.docs[0].data();
-    req.username = userSnap.username;
+    req.username = userData.username;
     next();
   } catch (error) {
     console.error("Lỗi xác thực:", error);
