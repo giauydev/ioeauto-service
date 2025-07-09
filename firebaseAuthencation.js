6675adbd6dae9f8b4d7b6034bf6e50a7701e29dd
iamgiauy <giauylethe@gmail.com>
2025-07-09 09:47:44 +0700

    Update firebaseAuthencation.js

--- firebaseAuthencation.js
+++ firebaseAuthencation.js
@@ -1,6 +1,12 @@
 const admin = require('firebase-admin');
 const db = admin.firestore(); 
-
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  })
+});
 async function verifyToken(req, res, next) {
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
